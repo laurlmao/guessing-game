0ed1c53cbc02a8aa99ad652417da411478a25801
lauren <laurenagee@seattleacademy.org>
2024-01-19 11:22:54 -0800

    yas

---
+++ script.js
@@ -0,0 +1,37 @@
+const numberBox = document.getElementById("guess");
+
+const outputBox = document.getElementById("output");
+
+const newNumber = Math.ceil(Math.random()*100);
+
+numberBox.addEventListener("change", checknumber);
+
+function checknumber() {
+    const guess = numberBox.value;
+
+    if (guess > newNumber) {
+        outputBox.innerText = "too high!";
+    }
+
+    else if (guess < newNumber) {
+        outputBox.innerText = "too low!";
+    }
+
+    else if (guess = newNumber) {
+        feedbackBox.innertext = "correct!";
+    }
+}
+
+const numberBox2 = document.getElementById("square");
+
+const outputBox2 = document.getElementById("output2");
+
+numberBox2.addEventListener("change", checknumber2);
+
+function checknumber2() {
+
+    const newValue = (numberBox2.value)**2; 
+
+    outputBox2.innerText = newValue;
+   
+}
